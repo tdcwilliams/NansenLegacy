8eb463599a6007bd67b7ca8f24cef9c5b3aa1101
tdcwilliams <tdcwilliams@gmail.com>
2020-02-14 00:11:41 +0100

    setup ensemble forecast run

--- scripts/ensemble/preprocessing/setup_ensemble_fc.sh
+++ scripts/ensemble/preprocessing/setup_ensemble_fc.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 # creates the config files for the member ensemble forecasts
 # NB no interaction between forecasts
-if [ $# -ne 1 ]
+if [ $# -eq 0 ]
 then
     echo Usage: $0 CONFIG_FILE
+    echo Usage: $0 CONFIG_FILE RESTART_FORECAST_DATE_DIR
     exit 1
 fi
 
 CONFIG_FILE=$1
+#RESTART_FORECAST_DATE_DIR=$OUTPUT_FORECAST_DATA_DIR/arome_3km_ec2_3h_smos.fram/20180308
+#RESTART_FORECAST_DATE_DIR=$OUTPUT_FORECAST_DATA_DIR/arome_3km_ec2_3h.fram/20180308
+RESTART_FORECAST_DATE_DIR=$2
+
 cfg_base=`basename $CONFIG_FILE`
-cfg_root=${NEXTSIM_ENV_ROOT_DIR}/config_files/${cfg_base%*.cfg}
+fcname=${cfg_base%*.cfg}
+cfg_root=${NEXTSIM_ENV_ROOT_DIR}/config_files/$fcname
 NUM_ENS=11
 
 for i_ens in `seq 1 $NUM_ENS`
@@ -20,4 +26,15 @@ do
     echo Making $cfg
     cp $CONFIG_FILE $cfg
     sed -i "s|ENS_MEM|$i_ens|g" $cfg
+
+    # if we have a restart, we make a fake forecast for the date
+    # before we want to start and link into the forecast dir
+    # -then we find the restart in the usual way
+    [[ -z "$RESTART_FORECAST_DATE_DIR" ]] && continue
+    mkdir -p $OUTPUT_FORECAST_DATA_DIR
+    fcdir=$OUTPUT_FORECAST_DATA_DIR/${fcname}.${ens_str}
+    mkdir -p $fcdir
+    cmd="ln -s $RESTART_FORECAST_DATE_DIR $fcdir"
+    echo $cmd
+    $cmd
 done
