d75113c70c7d9127e671ee3c21f68cd605ee9ccb
tdcwilliams <tdcwilliams@gmail.com>
2020-02-14 17:31:25 +0100

    try again with this version of make_plots.sh

--- scripts/ensemble/postprocessing/make_plots.slurm.sh
+++ scripts/ensemble/postprocessing/make_plots.slurm.sh
@@ -1,32 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --account=nn2993k
 #SBATCH --job-name=make_arome_plots
 #SBATCH --nodes=1
-#SBATCH --ntasks=32
+#SBATCH --ntasks-per-node=16
+#SBATCH --cpus-per-task=2
 #SBATCH --qos=devel
 #SBATCH --time=0-0:30:00
 ##SBATCH --qos=short
 ##SBATCH --time=0-2:00:00
+#SBATCH --output=logs/slurm.%j.out
 
 cd $SLURM_SUBMIT_DIR
 source $HOME/nextsimf.ensemble.src
-
 function plot {
-    plot_nextsim_output.py $1 plot.cfg -o $fcdir/figs --no-im
+    figdir=$1/figs
+    [[ -d $figdir ]] && return
+    plot_nextsim_output.py $1 plot.cfg -o $figdir --no-im
 }
 
-
+prefix=$OUTPUT_FORECAST_DATA_DIR/arome_3km_ec2_arome_ensemble
 for smos in "" "_smos"
 do
-    fcdir_prefix=$OUTPUT_FORECAST_DATA_DIR/arome_3km_ec2_arome_ensemble${smos}.fram
-    for i in `seq 1 11`
+    for fcdir in ${prefix}${smos}.fram.mem_???/201?????
     do
-        s=`printf '%.3i' $i`
-        root_dir=${fcdir_prefix}.mem_$s
-        for fcdir in $root_dir/201?????
-        do
-            srun --ntasks=1 --exclusive ./make_plots.sh $fcdir &
-        done
+        srun --ntasks=1 plot $fcdir &
     done
 done
 
