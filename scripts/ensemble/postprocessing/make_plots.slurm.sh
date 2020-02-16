836fbe059a763521e1557d85eec563baa3a6e473
tdcwilliams <tdcwilliams@gmail.com>
2020-02-16 22:17:29 +0100

    slurm plotting

---
+++ scripts/ensemble/postprocessing/make_plots.slurm.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+sbatch_opts=()
+sbatch_opts+=("--job-name=arome_plots")
+sbatch_opts+=("--time=00:30:00")
+sbatch_opts+=("--mem-per-cpu=5G")
+
+prefix=$OUTPUT_FORECAST_DATA_DIR/arome_3km_ec2_arome_ensemble
+for smos in "" "_smos"
+do
+    for fcdir in ${prefix}${smos}.fram.mem_???/201?????
+    do
+        cmd="plot_nextsim_output.py \
+            $fcdir plot.cfg \
+            -o $fcdir/figs --no-im"
+        sbatch \
+            ${sbatch_opts[@]} \
+            --export=COMMAND="$cmd" \
+            slurm_batch_job.sh
+    done
+done
