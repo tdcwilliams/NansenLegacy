2a22230ba6a96315afa6829de630a80f348b4d79
tdcwilliams <tdcwilliams@gmail.com>
2021-04-06 20:52:44 +0200

    update runtime scripts

--- scripts/ensemble/runtime/rerun_ensemble_forecast.sh
+++ scripts/ensemble/runtime/rerun_ensemble_forecast.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # creates the config files for the member ensemble forecasts
 # NB no interaction between forecasts
-if [ $# -ne 4 ]
-then
+function usage {
     echo Usage: $0 FCNAME DATE1 DATE2 DURATION
     exit 1
-fi
+}
+[[ $# -ne 4 ]] && usage
 
 FCNAME=$1
 DATE1=$2
@@ -14,9 +14,11 @@ DURATION=$4
 NUM_ENS=11
 #anr="--allow-no-restart"
 
-for i_ens in `seq 1 $NUM_ENS`
+for i_ens in $(seq 1 $NUM_ENS)
 do
-    ens_str=`printf "mem_%.3d" "$i_ens"` #eg mem_001
-    rerun_forecast.py ${FCNAME}.${ens_str} $DATE1 $DATE2 $DURATION \
-        -np --hpc $anr
+    ens_str=$(printf "mem_%.3d" "$i_ens") #eg mem_001
+    fcname="${FCNAME}.${ens_str}"
+    exe=$OUTPUT_FORECAST_DATA_DIR/$fcname/nextsim.exec
+    rerun_forecast.py $fcname $DATE1 $DATE2 $DURATION \
+        -ds -1 -c -e $exe --hpc $anr
 done
--- scripts/ensemble/runtime/run_legacy_forecast.slurm.sh
+++ scripts/ensemble/runtime/run_legacy_forecast.slurm.sh
@@ -15,46 +15,20 @@
 #SBATCH --cpus-per-task=1
 
 ## Queue Option (preproc for 1 node; normal for >4 nodes; also can be bigmem)
-#SBATCH --qos=QOS
+#QOS_LINE
 
 ## Email info
 #SBATCH --mail-type=ALL   # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=EMAIL # email to the user
 
-source $HOME/nextsimf.ensemble.src
+source $HOME/pynextsimf.src
 
 # set options for run_forecast.py
-# - capitalised names to be substituted in run_forecast_hpc.py
+# - capitalised names to be substituted in submit_forecast_hpc.py
 forecast_dir="FORECAST_DIR"
-forecast_name="FORECAST_NAME"
-date_begin="DATE_BEGIN"
-duration="DURATION"
-allow_no_restart="ALLOW_NO_RESTART"
-date_shift="DATE_SHIFT"
-no_post_proc="NO_POST_PROC"
-exec_name="EXEC_NAME"
-#mumps_memory="MUMPS_MEMORY"
-mumps_memory="2000"
+pyargs="PYARGS"
 
 #run forecast
 logdir=$forecast_dir/logs
-cd $SCRATCH
-cp -a $exec_name .
-pwd
-ls
-
-forecast_preproc.py $forecast_name $date_begin $duration \
-        $allow_no_restart \
-        --assimilation-date-shift $date_shift \
-        $no_post_proc \
-        > $logdir/forecast_preproc.log || exit 1
-
-mpirun ./`basename $exec_name` -mat_mumps_icntl_23 $mumps_memory \
-    --config-files=$forecast_dir/inputs/nextsim.cfg \
-    &> $logdir/nextsim.${SLURM_JOB_ID}.log || exit 1
-
-forecast_postproc.py $forecast_name $date_begin $duration \
-        $allow_no_restart \
-        --assimilation-date-shift $date_shift \
-        $no_post_proc \
-        > $logdir/forecast_postproc.log || exit 1
+mkdir -p $logdir
+run_forecast.py $pyargs &> $logdir/run_forecast.log || exit 1
