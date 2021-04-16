b3bd0125cf045848350961fea02fb1ed534df898
tdcwilliams <tdcwilliams@gmail.com>
2021-04-16 06:45:04 +0200

    evaluate individual forecasts and "analyses" as well as true analysis

--- scripts/ensemble/postprocessing/evaluate_forecasts.sh
+++ scripts/ensemble/postprocessing/evaluate_forecasts.sh
@@ -1,36 +1,47 @@
-#! /bin/bash
-function check_and_run_usage {
-    echo check_and_run COMMAND OUTPUTDIR
-}
-function run_usage {
-    echo run COMMAND
-}
-function check_and_run {
-    [[ $# -ne 2 ]] && { check_and_run_usage; exit 1; }
-    [[ -d $2 ]] && { echo "$2 exists"; return; } # don't repeat evaluations
-    run "$1 -o $2"
+#! /bin/bash -x
+set -e
+
+function usage {
+    echo "evaluate_forecasts.sh ROOT_DIR FCNAME [MOORINGS_MASK]"
 }
 function run {
-    [[ $# -ne 1 ]] && { run_usage; exit 1; }
     echo $1
     sbatch \
         ${sbatch_opts[@]} \
         --export=COMMAND="$1" \
         slurm_batch_job.sh
 }
+function check_and_run {
+    [[ -d $2 ]] && { echo "$2 exists"; return; } # don't repeat evaluations
+    run "$1 -o $2"
+}
+
+ROOT_DIR=$1
+FCNAME=$2
+MOORINGS_MASK=${3-"Moorings.nc"}
+dir_prefix="${ROOT_DIR}/${FCNAME}.mem_"
 
 sbatch_opts=()
 sbatch_opts+=("--job-name=eval-fc")
 sbatch_opts+=("--time=00:15:00")
 
-prefix=$OUTPUT_FORECAST_DATA_DIR/arome_3km_ec2_arome_ensemble
-for smos in "" "_smos"
+for fcdir in ${dir_prefix}???
 do
-    for fcdir in ${prefix}${smos}.fram.mem_???/201?????
-    do
-        check_and_run "evaluate_forecast.py $fcdir -s OsisafConc" $fcdir/eval-osisaf-conc
-            #sf=0 better for looking at ice edge
-        check_and_run "evaluate_forecast.py $fcdir -s SmosThick" $fcdir/eval-smos
-            #sf>0 doesn't add anything
-    done
+    # evaluate "analysis" for each member
+    ./eval_freerun.sh $fcdir/analysis "Moorings_%Y%m%d.nc" 1 1 1 1
+done
+
+# evaluate true analysis
+# - no drift eval implemented yet
+#./eval_freerun.sh $ROOT_DIR/analysis "Moorings_%Y%m%d.nc" 1 0 1 1
+
+for fcdir in ${dir_prefix}???/201?????
+do
+    fcopts="$fcdir -mm $MOORINGS_MASK"
+    fcopts2="-s OsisafConc -sig 10"
+    check_and_run "evaluate_forecast.py $fcopts $fcopts2" "$fcdir/eval-osisaf-conc"
+        #sig=0 better for looking at ice edge
+    fcopts2="-s SmosThick -sig 10"
+    check_and_run "evaluate_forecast.py $fcopts $fcopts2" "$fcdir/eval-smos"
+        #sig>0 doesn't add anything
 done
